f5cad10c76c9c57261d145480dae50185f67fb21
Angelina Velinska <angelina.velinska@coremedia.com>
2013-02-14 10:23:08 +0100

    moved the tracker classes to a coremedia friendly package :)

--- src/main/java/com/coremedia/nexus/plugins/ga/GaTrackerRepositoryCustomizer.java
+++ src/main/java/com/coremedia/nexus/plugins/ga/GaTrackerRepositoryCustomizer.java
@@ -1,4 +1,4 @@
-package org.mule.nexus.plugins.ga;
+package com.coremedia.nexus.plugins.ga;
 
 
 import org.slf4j.Logger;
@@ -10,7 +10,6 @@ import org.sonatype.nexus.proxy.repository.RequestProcessor;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.Set;
 
 /**
  * Small customizer, which only adds the GA tracker to the repositories.
@@ -18,16 +17,14 @@ import java.util.Set;
  * as those don't get called if they are in a group (which all of them are)
  * we'll add this to all.
  *
- * @author Lars J. Nilsson
- * @author avelinsk
  */
 public class GaTrackerRepositoryCustomizer implements RepositoryCustomizer {
 
-  static {
+  private static final Logger LOG = LoggerFactory.getLogger(GaTrackerRepositoryCustomizer.class);
+
+  public GaTrackerRepositoryCustomizer() {
     PropertiesLoader.loadProperties();
   }
-
-  private final Logger log = LoggerFactory.getLogger(getClass());
 
   @Inject
   @Named("gaTracker")
@@ -36,7 +33,7 @@ public class GaTrackerRepositoryCustomizer implements RepositoryCustomizer {
   public void configureRepository(Repository rep) throws ConfigurationException {
     for (String s : PropertiesLoader.REPOSITORIES) {
       if (s.equalsIgnoreCase(rep.getId())) {
-        log.info("Attaching Google Analytics tracker to: " + rep.getName());
+        LOG.info("Attaching Google Analytics tracker to: " + rep.getName());
         rep.getRequestProcessors().put("gaTracker", gaTracker);
       }
     }
@@ -44,7 +41,7 @@ public class GaTrackerRepositoryCustomizer implements RepositoryCustomizer {
 
   public boolean isHandledRepository(Repository rep) {
     boolean handlesRepository = PropertiesLoader.REPOSITORIES.contains(rep.getId());
-    log.info("Handles repository '" + rep.getName() + "': " + handlesRepository);
+    LOG.debug("Handles repository '" + rep.getName() + "': " + handlesRepository);
     return handlesRepository;
   }
 }
--- src/main/java/com/coremedia/nexus/plugins/ga/GaTrackerRequestProcessor.java
+++ src/main/java/com/coremedia/nexus/plugins/ga/GaTrackerRequestProcessor.java
@@ -1,4 +1,4 @@
-package org.mule.nexus.plugins.ga;
+package com.coremedia.nexus.plugins.ga;
 
 
 import com.boxysystems.jgoogleanalytics.FocusPoint;
@@ -6,16 +6,19 @@ import com.boxysystems.jgoogleanalytics.JGoogleAnalyticsTracker;
 import com.boxysystems.jgoogleanalytics.LoggingAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sonatype.nexus.proxy.AccessDeniedException;
+import org.sonatype.nexus.proxy.IllegalOperationException;
+import org.sonatype.nexus.proxy.ItemNotFoundException;
+import org.sonatype.nexus.proxy.RequestContext;
 import org.sonatype.nexus.proxy.ResourceStoreRequest;
 import org.sonatype.nexus.proxy.access.Action;
 import org.sonatype.nexus.proxy.item.AbstractStorageItem;
+import org.sonatype.nexus.proxy.item.StorageItem;
 import org.sonatype.nexus.proxy.repository.ProxyRepository;
 import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.nexus.proxy.repository.RequestProcessor;
 
 import javax.inject.Named;
-import java.io.IOException;
-import java.util.Properties;
 
 /**
  * This processor reacts on repository "read" actions and track
@@ -33,26 +36,32 @@ import java.util.Properties;
 @Named("gaTracker")
 public class GaTrackerRequestProcessor implements RequestProcessor {
 
-  static {
-    PropertiesLoader.loadProperties();
-  }
-
-  private final Logger log = LoggerFactory.getLogger(getClass());
   private final JGoogleAnalyticsTracker tracker;
-  private String userName = "CoreMedia";
+  private static final String REQUEST_USER = "request.user";
+  private static final String REQUEST_AGENT = "request.agent";
+  private static final String DEFAULT_USERNAME = "CoreMedia";
+
+  private final Logger LOG = LoggerFactory.getLogger(getClass());
 
   public GaTrackerRequestProcessor() {
-    if (checkGaTrackerId()) {
-      log.info("Creating new Google Analytics tracker with id: " + PropertiesLoader.GA_TRACKER_ID);
-      tracker = new JGoogleAnalyticsTracker("Nexus", "2.2", PropertiesLoader.GA_TRACKER_ID, userName);
-      adaptLogging();
-    } else {
-      log.warn("Google Analytics tracking is disabled.");
-      tracker = new JGoogleAnalyticsTracker("Nexus", "2.2", "", userName);
-    }
+    PropertiesLoader.loadProperties();
+    tracker = new JGoogleAnalyticsTracker("Nexus", PropertiesLoader.NEXUS_VERSION, PropertiesLoader.GA_TRACKER_ID);
+    LOG.info("Creating new Google Analytics tracker with id: " + PropertiesLoader.GA_TRACKER_ID);
+    adaptLogging();
   }
 
   public boolean process(Repository rep, ResourceStoreRequest req, Action action) {
+    String userName = "";
+    RequestContext requestContext = req.getRequestContext();
+
+    //TODO: remove for loop and logging when fixed
+    LOG.info("Request Context: \n");
+    for (String s : requestContext.keySet()) {
+      LOG.info("key=" + s  + "   value=" + requestContext.get(s));
+    }
+
+    userName = requestContext.containsKey(REQUEST_USER) ? (String) requestContext.get(REQUEST_USER) : DEFAULT_USERNAME;
+
     if (action == Action.read) {
       /*
        * 1) create path by appending repo path to repo id
@@ -60,26 +69,27 @@ public class GaTrackerRequestProcessor implements RequestProcessor {
        * 3) track asynchronously, this will perform the tracking on a new thread
        */
       String path = rep.getId() + req.getRequestPath();
-      log.debug("Tracking path: " + path);
-      FocusPoint p = new Point(path);
-      tracker.trackAsynchronously(p);
-      String trackerId = System.getProperty("nexus.ga.trackerId");
-      if (trackerId != null) {
-        log.debug("Tracker ID: " + trackerId);
-      }
+      LOG.debug("Tracking path: " + path);
+      FocusPoint focusPoint = new Point(path, userName);
+      tracker.trackAsynchronously(focusPoint);
     } else {
-      log.debug("Ingoring request of action '" + action + "' for: " + req.getRequestPath());
+      LOG.debug("Ingoring request of action '" + action + "' for: " + req.getRequestPath());
     }
     return true;
   }
 
+  public boolean shouldRetrieve(Repository repository, ResourceStoreRequest request, StorageItem item) throws IllegalOperationException, ItemNotFoundException, AccessDeniedException {
+    LOG.debug("Returns true for 'retrieve', on item: " + item.getPath());
+    return true;
+  }
+
   public boolean shouldCache(ProxyRepository proxy, AbstractStorageItem item) {
-    log.debug("Returns true for 'cache', on item: " + item.getPath());
+    LOG.debug("Returns true for 'cache', on item: " + item.getPath());
     return true;
   }
 
   public boolean shouldProxy(ProxyRepository proxy, ResourceStoreRequest req) {
-    log.debug("Returns true for 'proxy', on request: " + req.getRequestPath());
+    LOG.debug("Returns true for 'proxy', on request: " + req.getRequestPath());
     return true;
   }
 
@@ -93,11 +103,11 @@ public class GaTrackerRequestProcessor implements RequestProcessor {
     tracker.setLoggingAdapter(new LoggingAdapter() {
 
       public void logMessage(String msg) {
-        log.debug(msg);
+        LOG.debug(msg);
       }
 
       public void logError(String msg) {
-        log.error(msg);
+        LOG.error(msg);
       }
     });
   }
@@ -114,8 +124,8 @@ public class GaTrackerRequestProcessor implements RequestProcessor {
    */
   private static class Point extends FocusPoint {
 
-    public Point(String name) {
-      super(name);
+    public Point(String name, String userName) {
+      super(name, userName);
     }
 
     @Override
